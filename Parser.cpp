e7981c65814e4a68bd94d9caea887e6093a6912b
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-22 14:40:38 -0400

    Update Parser.cpp

--- Parser.cpp
+++ Parser.cpp
@@ -24,3 +24,28 @@ public:
         return ast;
     }
 };
+
+enum class TokenType {
+    DEFINE_FUNCTION, RETURN, CALL_FUNCTION
+};
+
+class Parser {
+    std::unordered_map<std::string, std::vector<std::string>> functions;
+
+public:
+    void parseFunction(std::vector<Token>& tokens) {
+        std::string functionName = tokens[1].value;
+        functions[functionName] = {};
+
+        for (size_t i = 2; i < tokens.size(); ++i) {
+            if (tokens[i].type == TokenType::END_FUNCTION) break;
+            functions[functionName].push_back(tokens[i].value);
+        }
+    }
+
+    void callFunction(std::string functionName, int arg) {
+        if (functions.find(functionName) != functions.end()) {
+            executeBinary(generateBinary(functions[functionName]));
+        }
+    }
+};
